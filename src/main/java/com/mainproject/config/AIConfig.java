a14f4e30a2bd157132b8b0d0369a4f20d7aeef94
Siddesh Rokade <159635585+Siddesh1105@users.noreply.github.com>
2026-08-17 19:53:18 +0530

    AI chatbot integration done

--- src/main/java/com/mainproject/config/AIConfig.java
+++ src/main/java/com/mainproject/config/AIConfig.java
@@ -3,7 +3,7 @@ package com.mainproject.config;
 public class AIConfig {
 
     public static final String API_KEY =
-            "AQ.Ab8RN6JdVH2bGCq7t52BYf-lCz2x_DkvcHi9HPKZmwuJAhVxwA";
+            "";
 
     public static final String MODEL =
             "gemini-3.5-flash-lite";
