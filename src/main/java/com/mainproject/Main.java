077f117cf5a8250c2666275549385c5fb71eea47
durgehsonawane28 <236364183+durgeshsonawane21@users.noreply.github.com>
2026-08-18 11:50:12 +0530

    Homepage and Icons set

--- src/main/java/com/mainproject/Main.java
+++ src/main/java/com/mainproject/Main.java
@@ -8,8 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Application.launch(HomeScreen.class, args);
-        System.out.print("hiii");
-        System.out.print("hiii");
-        System.out.print("hiii");
+        
     }
 }
