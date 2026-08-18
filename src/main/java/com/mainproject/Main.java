5a8bb291b0de66d376bc2d1c50679c102be0797a
durgehsonawane28 <236364183+durgeshsonawane21@users.noreply.github.com>
2026-08-18 17:54:04 +0530

    main sop

--- src/main/java/com/mainproject/Main.java
+++ src/main/java/com/mainproject/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Application.launch(HomeScreen.class, args);
+        System.out.println("hiii");
     }
 }
